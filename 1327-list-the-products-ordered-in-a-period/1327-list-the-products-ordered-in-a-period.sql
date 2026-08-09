33b37fbd98f40ed6eb79847532d383b6b9f80567
Priyam Patil <patilpriyam03@gmail.com>
2026-08-09 21:01:49 +0530

    Time: 804 ms (45.95%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,17 @@
+
+select product_name,unit
+from
+
+(select p.product_name,sum(o.unit) as unit
+from Products as p
+INNER JOIN
+Orders as o
+on p.product_id=o.product_id
+where extract(year_month from o.order_date) =202002 
+group by p.product_name) as derived_table
+
+where unit>=100;
+
+
+
+
