266e2b652c975c38b8496184306eb0e63c753481
Priyam Patil <patilpriyam03@gmail.com>
2026-08-09 14:30:55 +0530

    Time: 334 ms (91.37%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,13 +1,27 @@
-select case 
--- odd id and must be < max(id)
-when id%2=1 AND id<(select MAX(id) from Seat) 
-then id+1
+-- select case 
+-- -- odd id and must be < max(id)
+-- when id%2=1 AND id<(select MAX(id) from Seat) 
+-- then id+1
 
--- even id
-when id%2=0
-then id-1
-else id
-end as id,
-student
+-- -- even id
+-- when id%2=0
+-- then id-1
+-- else id
+-- end as id,
+-- student
+-- from Seat
+-- order by id ;
+
+
+
+-- OR
+
+select case 
+when id=(select MAX(id) from Seat) AND id%2=1 then id
+when id%2=1 then id+1 else id-1 end
+as id,student 
 from Seat
-order by id ;
+order by id;
+
+
+
