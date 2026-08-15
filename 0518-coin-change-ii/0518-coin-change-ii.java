a6fa72539fe957e7eee5c460adebeac897ab80d8
Priyam Patil <patilpriyam03@gmail.com>
2026-08-15 09:18:52 +0530

    Time: 11 ms (60.48%), Space: 56.8 MB (37.75%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,26 @@
+// variation of unbounded knapsack
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                if (coins[i - 1] <= j) {
+                    // valid case
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                } else {
+                    // invalid case
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+
+    }
+}
