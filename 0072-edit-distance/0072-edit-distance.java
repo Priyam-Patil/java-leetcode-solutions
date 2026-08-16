19b3c4116f08b444e1eaa038f81237af2417d2f5
Priyam Patil <patilpriyam03@gmail.com>
2026-08-16 09:53:57 +0530

    Time: 6 ms (8.64%), Space: 47.3 MB (35.59%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,35 @@
+// variation of LCS(longest common subsequence)
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int dp[][] = new int[n + 1][m + 1];
+        // if either word1 or word2 is empty then
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                // word1 is empty
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                // word2 is empty
+                if (j == 0) {
+                    dp[i][j] = i;
+                }
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    // same
+                    dp[i][j] = dp[i - 1][j - 1];//shrink both
+                } else {
+                    // different
+                    dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                    // Math.min(add,Math.min(delete,replace))+1;
+                }
+            }
+
+        }
+        return dp[n][m];
+    }
+}
