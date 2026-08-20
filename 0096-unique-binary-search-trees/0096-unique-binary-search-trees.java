53c15cc3549944fe3361929e6f676c25000e5057
Priyam Patil <patilpriyam03@gmail.com>
2026-08-20 16:32:04 +0530

    Time: 0 ms (100%), Space: 41.9 MB (84.72%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,13 @@
+// variation of catalan number sequence
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;//for nodes=0-->{null bst}
+        for (int i = 2; i <= n; i++) {//Ci
+            for (int j = 0; j <= i - 1; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+}
