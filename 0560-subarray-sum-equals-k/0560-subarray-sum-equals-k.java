6a820ac7d26707e33394729a0906cbe560637dac
Priyam Patil <patilpriyam03@gmail.com>
2026-08-27 00:06:48 +0530

    Time: 23 ms (93.63%), Space: 49.2 MB (12.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,3 +1,4 @@
+
 // hashmap+prefix sum count
 import java.util.*;
 
@@ -16,3 +17,23 @@ class Solution {
         return count;
     }
 }
+
+
+
+// import java.util.*;
+// class Solution {
+//     public int subarraysWithXorK(int[] nums, int k) {
+//       HashMap<Integer,Integer> map=new HashMap<>();
+//       map.put(0,1);
+//       int xor=0;
+//       int count=0;
+//       for(int i=0;i<nums.length;i++){
+//         xor=xor^nums[i];
+//         if(map.containsKey(xor^k)){
+//             count+=map.get(xor^k);
+//         }
+// map.put(xor,map.getOrDefault(xor,0)+1);
+//       }
+//       return count;
+//     }
+// }
