71296057fde869cafba8187815a04ff7e1ef73f3
Priyam Patil <patilpriyam03@gmail.com>
2026-08-27 19:32:42 +0530

    Time: 2 ms (94.51%), Space: 48 MB (16.79%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,21 +1,21 @@
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] ans = new int[2];
-        Arrays.sort(nums);
-        int duplicate = -1;
-        int missing = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                duplicate = nums[i];
-            } else if (nums[i] > nums[i - 1] + 1) {
-                missing = nums[i - 1] + 1;
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        int missing = -1;
+        int repeated = -1;
+        for (int i = 0; i < n; i++) {
+            freq[nums[i]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) {
+                repeated = i;
+            }
+            if (freq[i] == 0) {
+                missing = i;
             }
         }
-        if (nums[nums.length - 1] != nums.length) {
-            missing = nums.length;
-        }
-        ans[0] = duplicate;
-        ans[1] = missing;
-        return ans;
+        return new int[] { repeated, missing };
     }
 }
