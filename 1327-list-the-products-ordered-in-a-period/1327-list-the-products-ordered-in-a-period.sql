ca7461f58e1b159be61f3239bb399494260fb84d
Priyam Patil <patilpriyam03@gmail.com>
2026-09-05 13:09:53 +0530

    Time: 941 ms (24.41%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,15 +1,6 @@
-
-
-select p.product_name,sum(o.unit) as unit
-from Products as p
-INNER JOIN
-Orders as o
+select p.product_name,sum(o.unit)as unit
+from Products as p inner join Orders as o
 on p.product_id=o.product_id
-where extract(year_month from o.order_date) =202002 
+where extract(year_month from o.order_date)=202002
 group by p.product_name
-having sum(o.unit)>=100;
-
-
-
-
-
+having sum(o.unit)>=100
