d8c4daed629ad0f49d3729556f5f7e05428c6114
Priyam Patil <patilpriyam03@gmail.com>
2026-09-05 12:47:07 +0530

    Time: 1916 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,6 +1,4 @@
--- delete the row having the larger ID.
-DELETE p1
-from Person as p1
-INNER JOIN Person as p2
-on p1.email=p2.email
-AND p1.id>p2.id;
+delete p1
+from Person as p1 inner join Person as p2
+on p1.email=p2.email and p1.id>p2.id;
+
